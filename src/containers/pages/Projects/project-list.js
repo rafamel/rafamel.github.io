2b7d85ccc74ecdfcedd81654adb49dd24fb478f0
Rafa Mel <melrcm@gmx.net>
2019-04-02 06:58:40 +0200

    updates projects

--- src/containers/pages/Projects/project-list.js
+++ src/containers/pages/Projects/project-list.js
@@ -1,16 +1,59 @@
 export default [
   {
+    name: 'poseup',
+    github: 'rafamel/poseup',
+    description: `Containerized development workflow for the masses.`,
+    icons: ['typescript', 'node', 'jest', 'travis', 'docker', 'babel']
+  },
+  {
+    name: 'shst (ongoing, unreleased)',
+    github: 'rafamel/shst',
+    description: `A shell parser for Javascript.`,
+    icons: ['typescript', 'lerna', 'jest', 'travis', 'docker', 'babel']
+  },
+  {
+    name: 'promist',
+    github: 'rafamel/promist',
+    description: `A dependable Promises and async utility belt.`,
+    icons: ['typescript', 'jest', 'travis', 'babel']
+  },
+  {
+    name: 'jorel (ongoing, unreleased)',
+    github: 'rafamel/jorel',
+    description: `The ultimate storage agnostic server and client ORM for GraphQL and offline-first.`,
+    icons: [
+      'typescript',
+      'lerna',
+      'graphql',
+      'knex',
+      'postgresql',
+      'sqlite',
+      'jest',
+      'travis',
+      'docker',
+      'babel'
+    ]
+  },
+  {
+    name: 'exits',
+    github: 'rafamel/exits',
+    description: `Run arbitrary functions & commands asynchronously before process termination, programatically or via CLI.`,
+    icons: ['typescript', 'node', 'jest', 'travis', 'babel']
+  },
+  {
     name: 'Sloth Pilot',
     url: 'https://slothpilot.surge.sh/',
-    description: `A basic routines app showcasing RxDB + rxdb-utils, kompi, and couchdb-proxy working together for an offline-first, fully reactive and synced PWA.`,
+    description: `A basic routines app showcasing a real-time, offline-first, and fully reactive PWA.`,
     icons: [
       'javascript',
       'parcel',
       'react',
+      'router',
       'auth0',
       'rxdb',
       'rxjs',
       'mobx',
+      'mst',
       'emotion',
       'materialui',
       'node',
@@ -21,52 +64,41 @@ export default [
     ]
   },
   {
+    name: 'OAS Test (alpha)',
+    github: 'rafamel/oas-test',
+    description: `Powerful OpenAPI 3.0 API testing, simplified.`,
+    icons: ['javascript', 'node', 'jest', 'travis', 'babel']
+  },
+  {
+    name: 'Ponds',
+    github: 'rafamel/ponds',
+    description: `Express middleware for better data and error handling per route.`,
+    icons: ['typescript', 'node', 'express', 'jest', 'travis', 'babel']
+  },
+  {
     name: 'rxdb-utils',
     github: 'rafamel/rxdb-utils',
     description: `RxDB's missing pieces. Includes plugins and utilities for RxDB allowing, amongst other things, for single remote couchdb databases to be used for all collections, subscribable getter definitions for models with query ensurances, better observable returning function definitions, and conflict resolution.`,
     icons: ['javascript', 'rxdb', 'rxjs', 'jest', 'travis', 'babel']
   },
   {
-    name: 'kompi (unreleased, ongoing)',
-    github: 'rafamel/kompi',
-    description: `Framework agnostic reactive component props composition. Currently only a React adapter is available. Builds on the precedent of recompose and proppy to create the swiss army knife of component props & component composition, with RxJS at its root.`,
-    icons: ['javascript', 'rxjs', 'react', 'lerna', 'jest', 'travis', 'babel']
+    name: 'Textantly',
+    github: 'rafamel/textantly',
+    description: `A Progressive Web Application made with React. Image edition and captions, powered by your browser.`,
+    icons: ['javascript', 'webpack', 'react', 'redux', 'materialui', 'babel']
   },
+
   {
-    name: 'couchdb-proxy (unreleased, ongoing)',
-    // github: 'rafamel/couchdb-proxy',
-    description: `A monorepo including a express-pluggable couchdb reverse proxy app, and a fully configured express application, for one-stop couchdb setup. Allows for custom authentication strategies and database action discrimination. Built to complement rxdb & rxdb-utils for a straightforwards infrastructure setup with CouchDB.`,
-    icons: ['node', 'express', 'couchdb', 'lerna', 'jest', 'travis', 'babel']
-  },
-  {
-    name: 'rxjs-utils (unreleased, ongoing)',
-    github: 'rafamel/rxjs-utils',
-    description: `RxJS 6 observable creation utils and operators.`,
-    icons: ['javascript', 'rxjs', 'jest', 'travis', 'babel']
-  },
-  {
-    name: 'electron-jamman',
-    github: 'rafamel/electron-jamman',
-    description: `An electron app to help out with JamMan Solo XT.`,
-    icons: ['javascript', 'sqlite', 'electron', 'babel']
-  },
-  {
-    name: 'aslug',
-    github: 'rafamel/aslug',
+    name: 'ts-react',
+    github: 'rafamel/ts-react',
     description:
-      "Slug a string while preserving its uniqueness. By default, it substitutes each forbidden character for its code, surrounded by a separator. The clearest use case is for ids that must be made safe, while assuring they won't conflict with other valid ids.",
-    icons: ['javascript', 'jest', 'travis', 'babel']
-  },
-  {
-    name: 'react-boilerplate',
-    github: 'rafamel/react-boilerplate',
-    description:
-      'Parcel w/ HMR, Babel, MobX & MST, Emotion, Material UI, Jest & Enzime. Packs default support for PWA, HMR, and contextual theming and store.',
+      'Webpack w/ Babel, MobX & MST, Emotion, Material UI, Jest & Enzime. Packs default support for PWA, and contextual theming and store.',
     icons: [
-      'javascript',
-      'parcel',
+      'typescript',
+      'webpack',
       'react',
       'mobx',
+      'mst',
       'emotion',
       'materialui',
       'jest',
@@ -78,25 +110,47 @@ export default [
     github: 'rafamel/rest-api-boilerplate',
     description:
       'ES2017, Express, Postgress, Objection.js, tokens, and Docker, ready for the win.',
-    icons: ['node', 'express', 'postgresql', 'objection', 'docker', 'babel']
+    icons: [
+      'javascript',
+      'node',
+      'express',
+      'postgresql',
+      'objection',
+      'knex',
+      'docker',
+      'babel'
+    ]
   },
   {
-    name: 'Textantly',
-    github: 'rafamel/textantly',
-    description: `A Progressive Web Application made with React. Image edition and captions, powered by your browser.`,
-    icons: ['javascript', 'webpack', 'react', 'redux', 'materialui', 'babel']
-  },
-  {
-    name: 'request-validation',
-    github: 'rafamel/request-validation',
-    description: `Express.js middleware for modular Joi request validations.`,
-    icons: ['node', 'express', 'jest', 'travis']
+    name: 'objection-before-and-unique',
+    github: 'rafamel/objection-before-and-unique',
+    description: `Advanced unique validation + Simpler before checks + Final schema validation for Objection.js.`,
+    icons: ['typescript', 'node', 'objection', 'knex', 'jest', 'travis']
   },
   {
     name: 'yaml-import',
     github: 'rafamel/yaml-import',
     description: `Import files and directories in YAML for a modular design.`,
-    icons: ['node', 'jest', 'travis']
+    icons: ['typescript', 'node', 'jest', 'travis']
+  },
+  {
+    name: 'slimconf',
+    github: 'rafamel/slimconf',
+    description: `A slim configuration util.`,
+    icons: ['typescript', 'jest', 'travis', 'babel']
+  },
+  {
+    name: 'aslug',
+    github: 'rafamel/aslug',
+    description:
+      "Slug a string while preserving its uniqueness. By default, it substitutes each forbidden character for its code, surrounded by a separator. The clearest use case is for ids that must be made safe, while assuring they won't conflict with other valid ids.",
+    icons: ['typescript', 'jest', 'travis', 'babel']
+  },
+  {
+    name: 'prefetchit',
+    github: 'rafamel/prefetchit',
+    description: `A naive prefetch for the browser.`,
+    icons: ['typescript', 'babel']
   },
   {
     name: 'joi-add',
@@ -105,23 +159,22 @@ export default [
     icons: ['javascript', 'babel', 'jest', 'travis']
   },
   {
-    name: 'objection-before-and-unique',
-    github: 'rafamel/objection-before-and-unique',
-    description: `Advanced unique validation + Simpler before checks + Final schema validation for Objection.js`,
-    icons: ['node', 'objection', 'jest', 'travis']
+    name: 'request-validation',
+    github: 'rafamel/request-validation',
+    description: `Express.js middleware for modular Joi request validations.`,
+    icons: ['javascript', 'node', 'express', 'jest', 'travis']
+  },
+  {
+    name: 'Print Ahoy',
+    github: 'rafamel/print-ahoy',
+    description: `Just an Electron app that prints all PDF files coming into a directory. Built to print on the host machine from a virtualized system when sending files to a shared folder.`,
+    icons: ['javascript', 'electron']
   },
   {
     name: 'Subtle Brackets',
     github: 'rafamel/subtle-brackets',
     description: `Underlined matching brackets and more for Visual Studio Code.`,
     icons: ['typescript', 'vscode']
-  },
-  {
-    name: 'Print Ahoy',
-    github: 'rafamel/print-ahoy',
-    description: `Just an Electron app that prints all PDF files coming into a directory. Built to print on the host machine from a virtualized
-    system when sending files to a shared folder.`,
-    icons: ['javascript', 'electron']
   },
   {
     name: 'Themelier (unreleased)',
@@ -135,4 +188,28 @@ export default [
     description: `Atom layout manager with Sublime Text-like shortcuts and commands featuring columns, rows, and grid.`,
     icons: ['javascript', 'atom']
   }
+  // {
+  //   name: 'Dalva (ongoing, unpublished)',
+  //   github: 'rafamel/dalva',
+  //   description: `Documents for the 21st Century`,
+  //   icons: ['javascript', 'jest', 'travis', 'babel']
+  // },
+  // {
+  //   name: 'proppy-extend',
+  //   github: 'rafamel/proppy-extend',
+  //   description: `Better component props composition for React.`,
+  //   icons: ['javascript', 'babel']
+  // },
+  // {
+  //   name: 'komfu (unreleased, ongoing)',
+  //   github: 'rafamel/komfu',
+  //   description: `Framework agnostic reactive component props composition. Currently only a React adapter is available. Builds on the precedent of recompose and proppy to create the swiss army knife of component props & component composition, with RxJS at its root.`,
+  //   icons: ['javascript', 'rxjs', 'react', 'lerna', 'jest', 'travis', 'babel']
+  // },
+  // {
+  //   name: 'couchdb-proxy (unreleased, ongoing)',
+  //   // github: 'rafamel/couchdb-proxy',
+  //   description: `A monorepo including a express-pluggable couchdb reverse proxy app, and a fully configured express application, for one-stop couchdb setup. Allows for custom authentication strategies and database action discrimination. Built to complement rxdb & rxdb-utils for a straightforwards infrastructure setup with CouchDB.`,
+  //   icons: ['javascript', node', 'express', 'couchdb', 'lerna', 'jest', 'travis', 'babel']
+  // }
 ];
